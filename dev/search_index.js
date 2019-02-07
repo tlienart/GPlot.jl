22de90bdfa17b68c1f42dab8acd7b6802fbd2d9f
autodocs <autodocs>
2019-02-07 02:49:19 +0000

    build based on 9f8d2b5

---
+++ dev/search_index.js
@@ -0,0 +1,35 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "GPlot.jl - Documentation",
+    "title": "GPlot.jl - Documentation",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#GPlot.jl-Documentation-1",
+    "page": "GPlot.jl - Documentation",
+    "title": "GPlot.jl - Documentation",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "#GPlot.Figure",
+    "page": "GPlot.jl - Documentation",
+    "title": "GPlot.Figure",
+    "category": "type",
+    "text": "Figure(id; opts...)\n\nReturn a new Figure object with name id, if a figure with that name exists already, return that object.\n\nNamed options:\n\nsize: a tuple (width, height) (for rendering prefer sizes ≥ (8, 8) see also fontsize recommendations)\nfont: a valid font name (note that if you use latex, this is irrelevant)\nfontsize: the master font size of the figure in pt (for rendering, prefer fontsizes ≥ 10pt)\ncol, color: the master font color of the figure (any Colors.Colorant can be used)\ntex, hastex, latex, haslatex: a boolean indicating whether there is LaTeX to be compiled in the figure\ntexscale: either fixed, none or scale to match the size of LaTeX expressions to the ambient fontsize (fixed and scale match, none doesn\'t)\npreamble, texpreamble: the LaTeX preamble, where you can change the font that is used and also make sure that the symbols you want to use are available.\nalpha, transparent, transparency: a bool indicating whether there may be transparent fillings in which case cairo is used\n\n\n\n\n\n"
+},
+
+{
+    "location": "#Figure-1",
+    "page": "GPlot.jl - Documentation",
+    "title": "Figure",
+    "category": "section",
+    "text": "Figure"
+},
+
+]}
