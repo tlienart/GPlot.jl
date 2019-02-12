fa7242f9869754d77404379b54535abfbe33a9bc
Thibaut Lienart <ltib@me.com>
2019-02-12 13:48:47 +0800

    fixing _ticks

--- src/ax_elem.jl
+++ src/ax_elem.jl
@@ -44,12 +44,9 @@ end
 #### [x|y|x2|y2]ticks
 ####
 
-function _ticks!(a::Option{Axes2D}, axsymb::Symbol, loc::AVR,
-                         lab::Option{Vector{String}}; overwrite=false,
-                         opts...)
-    isdef(a) || (a = add_axes2d!())
-    prefix = "$axsymb"
-    axis = getfield(a, Symbol(prefix * "axis"))
+function _ticks!(a::Axes2D, axs::Symbol, loc::Vector{Float64},
+                 lab::Option{Vector{String}}; overwrite=false, opts...)
+    axis = getfield(a, axs)
     locf = convert(Vector{Float64}, loc)
     # if overwrite, clear the current ticks object
     overwrite && clear!(axis.ticks)
@@ -66,17 +63,19 @@ function _ticks!(a::Option{Axes2D}, axsymb::Symbol, loc::AVR,
     set_properties!(axis.ticks; opts...)
     return a
 end
+_ticks!(::Nothing, a...; o...) = _ticks!(add_axes2d!(), a...; o...)
 
 # Generate xticks!, xticks, and associated for each axis
 for axs ∈ ["", "x", "y", "x2", "y2"]
-    f!  = Symbol(axs * "ticks!")
-    f   = Symbol(axs * "ticks")
+    f! = Symbol(axs * "ticks!")
+    f  = Symbol(axs * "ticks")
     ex = quote
-        $f!(a, loc, lab=∅; opts...)   = _ticks!(a, Symbol($axs), loc, lab; opts...)
-        $f!(loc::AVR, lab=∅; opts...) = $f!(gca(), loc, lab; opts...)
+        $f!(a, loc::AVR, lab=∅; opts...) =
+            _ticks!(a, Symbol($axs * "axis"), convert(Vector{Float64}, loc), lab; opts...)
+        $f!(loc::AVR, lab=∅; opts...) = $f!(gca(), convert(Vector{Float64}, loc), lab; opts...)
         # overwrite
-        $f(a, loc, lab=∅; opts...)   = $f!(a, loc, lab; overwrite=true, opts...)
-        $f(loc::AVR, lab=∅; opts...) = $f!(gca(), loc, lab; overwrite=true, opts...)
+        $f(a, loc::AVR, lab=∅; opts...) = $f!(a, loc, lab; overwrite=true, opts...)
+        $f(loc::AVR, lab=∅; opts...)    = $f!(gca(), loc, lab; overwrite=true, opts...)
     end
     eval(ex)
 end
@@ -91,14 +90,14 @@ end
 Update the properties of an existing legend object present on `axes`. If none
 exist then a new one is created with the given properties.
 """
-function legend!(a::Option{Axes2D}; overwrite=false, opts...)
+function legend!(a::Axes2D; overwrite=false, opts...)
     isdef(a) || (a = add_axes2d!())
     # if there exists a legend object but overwrite, then reset it
     (!isdef(a.legend) || overwrite) && (a.legend = Legend())
     set_properties!(a.legend; opts...)
     return a
 end
-
+legend!(::Nothing; opts...) = legend!(add_axes2d!(); opts...)
 legend!(; opts...) = legend!(gca(); opts...)
 
 """
