c6a7b35c8d842e99c4482aeaa03e6a2caaf35c45
Thibaut Lienart <tlienart@me.com>
2019-02-25 16:20:27 +1100

    more version problems...bis

--- test/_test.jl
+++ test/_test.jl
@@ -6,6 +6,10 @@ notisin(s, str) = @test !occursin(str, s)
 
 continuous_preview(false)
 
+if VERSION < v"1.1"
+    eachrow(A::AbstractArray) = (view(A, j, :) for j ∈ axes(A, 1))
+end
+
 function checkzip(z, v::VecOrMat)
     for (zi, vi) ∈ zip(z, eachrow(v))
       all(zi[j]==vi[j] for j ∈ eachindex(vi)) || return false
